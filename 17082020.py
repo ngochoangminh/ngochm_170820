08aa3337fd5ff87504eab12119056644215eae82
ngochoangminh <ngochoangminh34@gmail.com>
2020-08-18 15:23:37 +0700

    mas

--- 17082020.py
+++ 17082020.py
@@ -3,13 +3,13 @@ from requests.auth import HTTPBasicAuth
 from selenium import webdriver
 
 url = 'http://45.79.43.178/source_carts/wordpress/wp-admin'
-user_url = "http://45.79.43.178/source_carts/wordpress/wp-admin/users.php"
+
 response = requests.get(url, auth = HTTPBasicAuth('admin', '123456aA')) 
- 
+response = requests.patch('http://45.79.43.178/source_carts/wordpress/wp-admin/users')
 # print(response.cookies.keys) 
-print(response.content) 
+print(response.headers['Link'])
 
-
+########### Selenium ############
 chrome_ex = r"/usr/bin/chromedriver"
 driver = webdriver.Chrome(executable_path=chrome_ex)
 driver.get(url)
@@ -18,9 +18,12 @@ password = driver.find_element_by_id("user_pass")
 username.send_keys("admin")
 password.send_keys("123456aA")
 driver.find_element_by_name("wp-submit").click()
-res = driver.get(user_url)
-res = driver.find_element_by_xpath("/html/body/div[1]/div[2]/div[2]/div[1]/div[4]")
-res = driver.find_element_by_xpath("//form")
-res = driver.find_element_by_xpath("//table//tbody//tr[@id='user-1']")
-res = driver.find_element_by_xpath("//td[2]")
-print(res)
+
+res = driver.get('http://45.79.43.178/source_carts/wordpress/wp-admin/users.php')
+driver.find_element_by_xpath("/html/body/div[1]/div[2]/div[2]/div[1]/div[4]")
+driver.find_element_by_xpath("//form//table//tbody")
+res1 = driver.find_elements_by_xpath("//td[@class='username column-username has-row-actions column-primary']//strong//a")
+res2 = driver.find_elements_by_xpath("//td[@class='name column-name']")
+for i in range(len(res1)):
+    if str(res1[i].text)=='admin':
+        print(str(res2[i].text))
